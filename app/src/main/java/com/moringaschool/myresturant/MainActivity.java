1d0a86045f7fea6dd5b5994ef7fa42529c75fede
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-06 07:21:00 +0300

    end of mondays work

---
+++ app/src/main/java/com/moringaschool/myresturant/MainActivity.java
@@ -0,0 +1,43 @@
+package com.moringaschool.myresturant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+//import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.mLocationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.mLocationEditText);
+        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+
+            //preparing java code to handle user input from locationInput
+
+    });
+    }
+}
---
+++ app/src/main/java/com/moringaschool/myresturant/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,4 @@
+package com.moringaschool.myresturant;
+
+public class MyRestaurantsArrayAdapter {
+}
